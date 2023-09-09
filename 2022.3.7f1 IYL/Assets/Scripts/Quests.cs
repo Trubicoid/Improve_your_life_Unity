e1b8f4bd35bfd3a494393007d1bb4b10f0716eba
Trubicoid <trubicoid@gmail.com>
2023-09-09 17:42:41 +0200

    quest script erstellt

--- 2022.3.7f1 IYL/Assets/Scripts/Quests.cs
+++ 2022.3.7f1 IYL/Assets/Scripts/Quests.cs
@@ -1,9 +1,17 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SUS : MonoBehaviour
 {
+
+
+
+    public bool quest_completed = false;
+    public static List<SUS> AvailableAs = new List<SUS>();
+
+    
     // Start is called before the first frame update
     void Start()
     {
