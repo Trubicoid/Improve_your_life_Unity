c516353f4a593a04ee4f412bb2171fc6973a4995
mrspitzchen <jiaming.jya@gmail.com>
2023-12-04 13:20:25 +0100

    bilder

--- 2023.1.17f1 IYL rebuild/Assets/Bilder/Imageloader.cs
+++ 2023.1.17f1 IYL rebuild/Assets/Bilder/Imageloader.cs
@@ -38,5 +38,117 @@ public class Imageloader : MonoBehaviour
         {
             this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
         }
+        if (StaticString.quest_id_daily_1 == 5)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+        }
+        if (StaticString.quest_id_daily_1 == 6)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+        }
+        if (StaticString.quest_id_daily_1 == 7)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+        }
+        if (StaticString.quest_id_daily_1 == 8)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image3;
+        }
+        if (StaticString.quest_id_daily_1 == 9)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
+        }
+        if (StaticString.quest_id_daily_1 == 10)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+        }
+        if (StaticString.quest_id_daily_1 == 11)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+        }
+        if (StaticString.quest_id_daily_1 == 12)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+        }
+        if (StaticString.quest_id_daily_1 == 13)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image3;
+        }
+        if (StaticString.quest_id_daily_1 == 14)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
+        }
+        if (StaticString.quest_id_daily_1 == 15)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+        }
+        if (StaticString.quest_id_daily_1 == 16)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+        }
+        if (StaticString.quest_id_daily_1 == 17)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+        }
+        if (StaticString.quest_id_daily_1 == 18)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image3;
+        }
+        if (StaticString.quest_id_daily_1 == 19)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
+        }
+        if (StaticString.quest_id_daily_1 == 20)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+        }
+        if (StaticString.quest_id_daily_1 == 21)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+        }
+        if (StaticString.quest_id_daily_1 == 22)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+        }
+        if (StaticString.quest_id_daily_1 == 23)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image3;
+        }
+        if (StaticString.quest_id_daily_1 == 24)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
+        }
+        if (StaticString.quest_id_daily_1 == 25)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+        }
+        if (StaticString.quest_id_daily_1 == 26)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+        }
+        if (StaticString.quest_id_daily_1 == 27)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+        }
+        if (StaticString.quest_id_daily_1 == 28)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image3;
+        }
+        if (StaticString.quest_id_daily_1 == 29)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
+        }
+        if (StaticString.quest_id_daily_1 == 30)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+        }
+        if (StaticString.quest_id_daily_1 == 31)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+        }
+        if (StaticString.quest_id_daily_1 == 32)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+        }
     }
 }
--- 2023.1.17f1 IYL rebuild/Assets/Quest manager/Static String.cs
+++ 2023.1.17f1 IYL rebuild/Assets/Quest manager/Static String.cs
@@ -1,4 +1,4 @@
-using System;
+    using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -43,8 +43,62 @@ public class StaticString : MonoBehaviour
     public static bool Weekly_4_completed;
 
 
-    public static List<string> different_quests = new List<string> { "fox", "Vogel", "Schmetterling", "Elefant", "Bär", "Du hast es geschafft! Du hast alle unsere Quests gemeistert!" };
-    public static List<string> different_weekly_quests = new List<string> { "Yone", "Ezreal", "Fizz", "Soraka", "Lulu", "Du hast es geschafft! Du hast alle unsere Quests gemeistert!" };
+    public static List<string> different_quests = new List<string> {
+  "Gehe alleine auf einen mind. 20-minütigen Spaziergang.",
+  "Gehe in den Wald oder einen Park und jogge 15 Minuten.",
+  "Dehne dich, indem du dich beugst und deine Füße versuchst zu berühren.",
+  "Dehne deine Hüfte, indem du einen Ausfallschritt machst.",
+  "Dehne dich für 15 Minuten.",
+  "Dehne deinen ganzen Körper.",
+  "Suche dir ein ruhiges Plätzchen und mache 20 Minuten Yoga.",
+  "Beginne eine Konversation mit einem Familienmitglied.",
+  "Sage dir selbst im Spiegel mind. 5 positive Eigenschaften über dich, die du gerne hättest oder schon hast.",
+  "Schreibe dir 5 positive Affirmationen auf ein Stück Papier auf.",
+  "Schreibe dir auf, wofür du dankbar bist.",
+  "Notiere dir, was dich gerade beschäftigt oder stresst.",
+  "Notiere dir 3 Dinge, für die du in diesem Moment dankbar bist.",
+  "Nimm dir einen Block & einen Stift und notiere dir 3 Dinge, die du im vergangenen Tag gut gemacht hast.",
+  "Lächle 3 Minuten lang, auch wenn dir dies womöglich merkwürdig erscheint.",
+  "Meditiere für 10 Minuten.",
+  "Mache eine Atemübung.",
+  "Lege dich hin und versuche nach und nach all deine Muskeln zu entspannen.",
+  "Nimm dir eine Auszeit, höre deine Lieblingsplaylist und konzentriere dich voll und ganz auf die Musik.",
+  "Schalte möglichst viele Push-Benachrichtigungen aus.",
+  "Lege das Smartphone für 1h weg und bewege dich.",
+  "Versuche etwas selbst zu bauen.",
+  "Suche dir ein Rezept aus dem Internet und backe etwas Neues.",
+  "Backe einen Kuchen/Muffins.",
+  "Versuche, etwas Neues zu backen und anschließend zu dekorieren.",
+  "Suche dir ein gesundes Rezept aus einem Kochbuch oder im Internet, welches du an diesem Tag zubereiten möchtest.",
+  "Kaufe dir Obst und stell dir einen Früchteteller als Snack zusammen.",
+  "Bereite dir einen Smoothie zu. Benutze dafür ausschließlich frische Zutaten.",
+  "Nimm dir einen Block und Stift, schließe die Augen und versuche zu malen, was du dir vorstellst.",
+  "Schau dir ein Zeichnungs-Tutorial auf dem Internet an und male dein Lieblingstier.",
+  "Setze dich auf eine Bank oder in ein Café und skizziere die Landschaft.",
+  "Bastle eine Sache, die dir gefällt. Als Inspiration kann Google helfen.",
+  "Erfinde eine Melodie auf einem Instrument.",
+  "Du hast es Geschaft! Du hast alle Quests erledigt!"
+};
+
+
+
+
+
+
+    public static List<string> different_weekly_quests = new List<string> {
+  "Beginne eine Konversation mit einem*r Freund*in, mit dem*der du lange nichts mehr zu tun hattest.",
+  "Gehe mit jemandem, den du lange nicht mehr gesehen hast, etwa ein Nachbar, spazieren.",
+  "Triff dich mit einem Freund oder Bekannten und skizziert euch gegenseitig.",
+  "Gehe in eine Bibliothek und lasse dich beraten.",
+  "Gehe mit jemandem einen Kaffee trinken.",
+  "Gehe ins Kino mit jemandem.",
+  "Unterhalte dich mit einer sympathischen, fremden Person.",
+  "Nimm dir eine kurze digitale Pause von 30 Minuten und lege alle elektronischen Geräte zur Seite.",
+  "Lies ein Buch.",
+  "Beginne eine neue Sprache auf einer App zu lernen.",
+  "Nimm dir jeden Tag für eine Woche 15 Minuten Zeit, um das Jonglieren zu erlernen.",
+        "Du hast es Geschaft! Du hast alle Quests erledigt!"
+};
     public static List<int> used_quests = new List<int> { -1 };
     public static List<int> used_weekly_quests = new List<int> { -1 };
 
