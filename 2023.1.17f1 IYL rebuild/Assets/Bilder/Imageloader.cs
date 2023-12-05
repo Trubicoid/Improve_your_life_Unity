0dc2aaa14b98e32e4c0cef127fff56e6a1fa0802
mrspitzchen <jiaming.jya@gmail.com>
2023-12-05 15:26:08 +0100

    Bild Placeholder
    Für die scrollbaren Bilder

--- 2023.1.17f1 IYL rebuild/Assets/Bilder/Imageloader.cs
+++ 2023.1.17f1 IYL rebuild/Assets/Bilder/Imageloader.cs
@@ -9,6 +9,34 @@ public class Imageloader : MonoBehaviour
     public Sprite new_image2;
     public Sprite new_image3;
     public Sprite new_image4;
+    public Sprite new_image5;
+    public Sprite new_image6;
+    public Sprite new_image7;
+    public Sprite new_image8;
+    public Sprite new_image9;
+    public Sprite new_image10;
+    public Sprite new_image11;
+    public Sprite new_image12;
+    public Sprite new_image13;
+    public Sprite new_image14;
+    public Sprite new_image15;
+    public Sprite new_image16;
+    public Sprite new_image17;
+    public Sprite new_image18;
+    public Sprite new_image19;
+    public Sprite new_image20;
+    public Sprite new_image21;
+    public Sprite new_image22;
+    public Sprite new_image23;
+    public Sprite new_image24;
+    public Sprite new_image25;
+    public Sprite new_image26;
+    public Sprite new_image27;
+    public Sprite new_image28;
+    public Sprite new_image29;
+    public Sprite new_image30;
+    public Sprite new_image31;
+    public Sprite new_image32;
     // Start is called before the first frame update
     void Start()
     {
@@ -40,115 +68,115 @@ public class Imageloader : MonoBehaviour
         }
         if (StaticString.quest_id_daily_1 == 5)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image5;
         }
         if (StaticString.quest_id_daily_1 == 6)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image6;
         }
         if (StaticString.quest_id_daily_1 == 7)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image7;
         }
         if (StaticString.quest_id_daily_1 == 8)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image3;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image8;
         }
         if (StaticString.quest_id_daily_1 == 9)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image9;
         }
         if (StaticString.quest_id_daily_1 == 10)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image10;
         }
         if (StaticString.quest_id_daily_1 == 11)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image11;
         }
         if (StaticString.quest_id_daily_1 == 12)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image12;
         }
         if (StaticString.quest_id_daily_1 == 13)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image3;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image13;
         }
         if (StaticString.quest_id_daily_1 == 14)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image14;
         }
         if (StaticString.quest_id_daily_1 == 15)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image15;
         }
         if (StaticString.quest_id_daily_1 == 16)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image16;
         }
         if (StaticString.quest_id_daily_1 == 17)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image17;
         }
         if (StaticString.quest_id_daily_1 == 18)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image3;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image18;
         }
         if (StaticString.quest_id_daily_1 == 19)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image19;
         }
         if (StaticString.quest_id_daily_1 == 20)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image20;
         }
         if (StaticString.quest_id_daily_1 == 21)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image21;
         }
         if (StaticString.quest_id_daily_1 == 22)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image22;
         }
         if (StaticString.quest_id_daily_1 == 23)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image3;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image23;
         }
         if (StaticString.quest_id_daily_1 == 24)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image24;
         }
         if (StaticString.quest_id_daily_1 == 25)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image25;
         }
         if (StaticString.quest_id_daily_1 == 26)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image26;
         }
         if (StaticString.quest_id_daily_1 == 27)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image27;
         }
         if (StaticString.quest_id_daily_1 == 28)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image3;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image28;
         }
         if (StaticString.quest_id_daily_1 == 29)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image4;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image29;
         }
         if (StaticString.quest_id_daily_1 == 30)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image30;
         }
         if (StaticString.quest_id_daily_1 == 31)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image1;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image31;
         }
         if (StaticString.quest_id_daily_1 == 32)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image2;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = new_image32;
         }
     }
 }
