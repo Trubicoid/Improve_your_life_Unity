3cdef647bf66cbbc918c6a4db46b0261d790996f
Trubicoid <trubicoid@gmail.com>
2023-12-18 11:09:51 +0100

    scroll limiter und bug fixes

---
+++ 2023.1.17f1 IYL rebuild/Assets/antiscroll/limiter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class limiter : MonoBehaviour
+{
+    public ScrollRect scrollRect;
+    public float maxScrollLimit = 0.78f;
+
+    void Update()
+    {
+        // Limit scrolling by clamping the vertical normalized position
+        scrollRect.verticalNormalizedPosition = Mathf.Clamp(scrollRect.verticalNormalizedPosition, maxScrollLimit, 1.025f);
+    }
+}
